779845ce3cde21fb6de7d12929d9af5bf1810f3a
PIYUSH KUMAR <93852522+piyushkumar24@users.noreply.github.com>
2023-11-13 07:46:21 +0530

    Update Signup.js

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -26,7 +26,7 @@ function Signup() {
             validationErrors.email === '' &&
             validationErrors.password === ''
         ) {
-            axios.post('http://localhost:8081/signup', values)
+            axios.post('https://sql-login-page-backend.onrender.com/signup', values)
                 .then((res) => {
                     navigate('/');
                 })
